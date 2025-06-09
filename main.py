6959a0cba14018796a55216afffeeb33ce311afa
DavidCalalang <dcalalan@purdue.edu>
2025-06-09 15:39:22 -0400

    Setup gemini call

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from google import genai
+from google.genai.types import HttpOptions
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+def gemini_request(prompt):
+    client = genai.Client(
+        vertexai=True, project=os.getenv('project_id'), location=os.getenv('location')
+    )
+
+    contents = prompt
+    response = client.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=contents
+    )
+
+    return response
+
 def main():
-    print("Hello from meal-planning!")
+    configure()
+
+    prompt = "Give me a recipe to use after a workout involving one hour of weight training."
+
+    output = gemini_request(prompt)
+    print(output.text)
 
 
 if __name__ == "__main__":
